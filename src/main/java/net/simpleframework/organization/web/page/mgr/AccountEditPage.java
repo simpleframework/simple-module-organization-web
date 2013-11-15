dc2bafc0d8c05db19224a8c7a971d9cdc0bc7de5
simpleframework <cknet@126.com>
2013-11-15 11:42:45 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/organization/web/page/mgr/AccountEditPage.java
+++ src/main/java/net/simpleframework/organization/web/page/mgr/AccountEditPage.java
@@ -16,7 +16,7 @@ import net.simpleframework.organization.web.page.AbstractAccountAttriPage;
  * Licensed under the Apache License, Version 2.0
  * 
  * @author 陈侃(cknet@126.com, 13910090885)
- *         http://code.google.com/p/simpleframework/
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public class AccountEditPage extends AbstractAccountAttriPage {
