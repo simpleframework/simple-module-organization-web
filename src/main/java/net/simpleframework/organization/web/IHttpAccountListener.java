420e8c65d193faf5520dc750d7fc8eff1fc83a96
simpleframework <cknet@126.com>
2015-01-04 13:56:34 +0800

    Signed-off-by: simpleframework <cknet@126.com>

---
+++ src/main/java/net/simpleframework/organization/web/IHttpAccountListener.java
@@ -0,0 +1,17 @@
+package net.simpleframework.organization.web;
+
+import net.simpleframework.organization.IAccountSession;
+import net.simpleframework.organization.LoginObject;
+
+/**
+ * Licensed under the Apache License, Version 2.0
+ * 
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
+ *         http://www.simpleframework.net
+ */
+public interface IHttpAccountListener {
+
+	void login(IAccountSession accountSession, LoginObject login);
+
+	void logout(IAccountSession accountSession);
+}
