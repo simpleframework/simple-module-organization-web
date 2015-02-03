9509d0c9fdce39f84ff84f0f2ccf8cd54bcf1559
CHENKAN <CHENKAN@CHENKAN-PC>
2015-02-03 18:33:02 +0800

    Signed-off-by: CHENKAN <CHENKAN@CHENKAN-PC>

--- src/main/java/net/simpleframework/organization/web/page2/DepartmentMgrTPage.java
+++ src/main/java/net/simpleframework/organization/web/page2/DepartmentMgrTPage.java
@@ -1,5 +1,7 @@
 package net.simpleframework.organization.web.page2;
 
+import net.simpleframework.mvc.PageParameter;
+
 /**
  * Licensed under the Apache License, Version 2.0
  * 
@@ -7,4 +9,16 @@ package net.simpleframework.organization.web.page2;
  *         http://www.simpleframework.net
  */
 public class DepartmentMgrTPage extends AbstractMgrTPage {
+
+	@Override
+	protected void onForward(final PageParameter pp) {
+		super.onForward(pp);
+
+	}
+
+	@Override
+	public String toContentHTML(final PageParameter pp) {
+		final StringBuilder sb = new StringBuilder();
+		return sb.toString();
+	}
 }
