d0a47e33d0e3b69a452ad78980cb67a129f46ab1
CHENKAN <CHENKAN@CHENKAN-PC>
2015-02-03 16:14:27 +0800

    Signed-off-by: CHENKAN <CHENKAN@CHENKAN-PC>

--- src/main/java/net/simpleframework/organization/web/page2/DepartmentMgrTPage.java
+++ src/main/java/net/simpleframework/organization/web/page2/DepartmentMgrTPage.java
@@ -1,12 +1,10 @@
 package net.simpleframework.organization.web.page2;
 
-import net.simpleframework.mvc.template.lets.Tabs_BlankPage;
-
 /**
  * Licensed under the Apache License, Version 2.0
  * 
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public class DepartmentMgrTPage extends Tabs_BlankPage {
+public class DepartmentMgrTPage extends AbstractMgrTPage {
 }
