7aeedb78b5613196aaa89436767b028bee034f4f
CHENK <CHENK@CHENK-PC>
2015-09-08 19:02:19 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/organization/web/page/mgr/org2/RoleMgr_MembersTPage.java
+++ src/main/java/net/simpleframework/organization/web/page/mgr/org2/RoleMgr_MembersTPage.java
@@ -3,7 +3,9 @@ package net.simpleframework.organization.web.page.mgr.org2;
 import java.io.IOException;
 import java.util.Map;
 
+import net.simpleframework.ado.query.IDataQuery;
 import net.simpleframework.mvc.PageParameter;
+import net.simpleframework.mvc.common.element.JS;
 import net.simpleframework.mvc.common.element.LinkButton;
 import net.simpleframework.mvc.common.element.SpanElement;
 import net.simpleframework.mvc.component.ComponentParameter;
@@ -11,7 +13,10 @@ import net.simpleframework.mvc.component.ui.tree.AbstractTreeHandler;
 import net.simpleframework.mvc.component.ui.tree.TreeBean;
 import net.simpleframework.mvc.component.ui.tree.TreeNode;
 import net.simpleframework.mvc.component.ui.tree.TreeNodes;
+import net.simpleframework.organization.Department;
+import net.simpleframework.organization.EDepartmentType;
 import net.simpleframework.organization.Role;
+import net.simpleframework.organization.RoleChart;
 import net.simpleframework.organization.web.page.mgr.OmgrUtils;
 
 /**
@@ -36,21 +41,35 @@ public class RoleMgr_MembersTPage extends AbstractOrgMgrTPage {
 			final String currentVariable) throws IOException {
 		final StringBuilder sb = new StringBuilder();
 		sb.append("<div class='RoleMgr_MembersTPage clearfix'>");
-		sb.append(" <div class='lnav'>");
+		sb.append("<table width='100%'><tr>");
+		sb.append(" <td class='lnav'>");
 		sb.append("  <div class='lbl'>");
+		final RoleChart rchart = OmgrUtils.getRoleChart(pp);
 		final Role r = OmgrUtils.getRole(pp);
 		if (r != null) {
-			sb.append(OmgrUtils.getRoleChart(pp)).append(SpanElement.NAV).append(r);
+			sb.append(rchart).append(SpanElement.NAV).append(r);
 		}
 		sb.append("  </div>");
 		sb.append("  <div id='idRoleMgr_MembersTPage_dept'></div>");
-		sb.append(" </div>");
-		sb.append(" <div class='rtbl'>");
+		sb.append(" </td>");
+		sb.append(" <td class='rtbl'>");
 		sb.append("  <div class='tbar'>");
-		sb.append(LinkButton.backBtn().corner());
+		String backUrl = uFactory.getUrl(pp, RoleMgrTPage.class);
+		if (rchart != null) {
+			backUrl += "?chartId=" + rchart.getId();
+		}
+		sb.append(LinkButton.backBtn().corner().setOnclick(JS.loc(backUrl)));
 		sb.append("  </div>");
 		sb.append("  <div id='idRoleMgr_MembersTPage_tbl'></div>");
-		sb.append(" </div>");
+		sb.append(" </td>");
+
+		sb.append("</tr></table>");
+		// sb.append(" <div >");
+		//
+		// sb.append(" </div>");
+		// sb.append(" <div >");
+		//
+		// sb.append(" </div>");
 		sb.append("</div>");
 		return sb.toString();
 	}
@@ -60,6 +79,27 @@ public class RoleMgr_MembersTPage extends AbstractOrgMgrTPage {
 		@Override
 		public TreeNodes getTreenodes(final ComponentParameter cp, final TreeNode parent) {
 			final TreeNodes nodes = TreeNodes.of();
+			final TreeBean treeBean = (TreeBean) cp.componentBean;
+			if (parent == null) {
+				final Department dept = getOrg2(cp);
+				if (dept != null) {
+					TreeNode treeNode = new TreeNode(treeBean, parent, "所有成员");
+					nodes.add(treeNode);
+					treeNode = new TreeNode(treeBean, parent, dept);
+					nodes.add(treeNode);
+				}
+			} else {
+				final Object dataObject = parent.getDataObject();
+				if (dataObject instanceof Department) {
+					final IDataQuery<Department> dq = _deptService.queryDepartments(
+							(Department) dataObject, EDepartmentType.department);
+					Department dept;
+					while ((dept = dq.next()) != null) {
+						final TreeNode treeNode = new TreeNode(treeBean, parent, dept);
+						nodes.add(treeNode);
+					}
+				}
+			}
 			return nodes;
 		}
 	}
